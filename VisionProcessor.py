924ec713e97f5bd05e10101c2e810621b5700331
Abigail Fox <raptorsteam1711@gmail.com>
2017-02-17 06:37:04 +0000

    tuned to actual target

--- VisionProcessor.py
+++ VisionProcessor.py
@@ -23,14 +23,14 @@ publish/return contours somehow (networktables??)
     def process(self, frame):
         try:
             blur_output = self.blur(frame)
-#           cv2.imshow('blur', blur_output)
-#           cv2.waitKey(0)
-#           cv2.destroyAllWindows()
+#            cv2.imshow('blur', blur_output)
+#            cv2.waitKey(0)
+#            cv2.destroyAllWindows()
 
-            hsv_output = self.hsv_threshold(blur_output)
-#           cv2.imshow('hsv threshold', hsv_output)
-#           cv2.waitKey(0)
-#           cv2.destroyAllWindows()
+            hsv_output = self.hsv_threshold(frame)
+#            cv2.imshow('hsv threshold', hsv_output)
+#            cv2.waitKey(0)
+#            cv2.destroyAllWindows()
 
             contours = self.find_contours(hsv_output, False)
             
@@ -44,14 +44,14 @@ publish/return contours somehow (networktables??)
 
     def blur(self, source):
         #using a median filter
-        return cv2.medianBlur(source, 9) #magic!
+        return cv2.medianBlur(source, 3) #magic!
     
 
     def hsv_threshold(self, source):
-        #current exposure setting on axis camera: 8
-        hue = [120.0, 150.0]
-        sat = [0, 255.0]
-        val = [0, 255.0]
+        #current exposure setting on axis camera: 0
+        hue = [120.0, 180.0]
+        sat = [90.0, 165.0]
+        val = [0.0, 90.0]
         #convert to HSV colorspace
         out = cv2.cvtColor(source, cv2.COLOR_BGR2HSV)
         return cv2.inRange(source, (hue[0], sat[0], val[0]), (hue[1], sat[1], val[1]))
@@ -76,14 +76,16 @@ publish/return contours somehow (networktables??)
         output = []
         min_solidity = 80
         max_solidity = 100
-        min_ratio = 2.3
-        max_ratio = 3.0
+        min_ratio = 2.0
+        max_ratio = 4.0
         for contour in input_contours:
             #(x, y) is the top left coordinate of the image
             x,y,w,h = cv2.boundingRect(contour)
             ratio = (float)(w) / h
-#            print("ratio")
-#            print(ratio)
+            print("ratio")
+            print(ratio)
+            print("o.g. width")
+            print((float)(w))
             if(ratio < min_ratio or ratio > max_ratio):
                 continue
             area = cv2.contourArea(contour)
@@ -91,8 +93,8 @@ publish/return contours somehow (networktables??)
             contour_area = cv2.contourArea(hull)
             if(contour_area > 0):
                 solid = 100 * area / cv2.contourArea(hull)
-#                print("solid")
-#                print(solid)
+                print("solid")
+                print(solid)
                 if (solid < min_solidity or solid > max_solidity):
                     continue
             else:
@@ -123,9 +125,11 @@ publish/return contours somehow (networktables??)
                         continue
                     else:
                         center_points = coordinate
+                        print("width")
+                        print((float)(w))
                 else:
                     break
-#            cv2.circle(image_underlay, (center_width, center_height), 1, (255, 0, 0), -1)
+            cv2.circle(image_underlay, (center_width, center_height), 1, (255, 0, 0), -1)
 #        cv2.imshow('centers', image_underlay)
 #        cv2.waitKey(0)
 #        cv2.destroyAllWindows()
--- pipeline-test.py
+++ pipeline-test.py
@@ -15,7 +15,14 @@ from networktables.util import ntproperty
 
 #proper networktables setup
 logging.basicConfig(level=logging.DEBUG)
-NetworkTables.initialize(server='10.17.11.76')
+connected = False
+while(connected == False):
+    try:
+        NetworkTables.initialize(server='10.17.11.76')
+        connected = True
+    except:
+        connected = False
+            
 
 #create the field to talk to on the network table
 class NTClient(object):
@@ -39,32 +46,39 @@ def get_image():
 #    cv2.imwrite("frame.jpg", img_array)
     return img_array
 
-def find_distance(width, height, y):
+def find_distance(width, y):
     #distances are in inches
-    KNOWN_WIDTH = 6.25
-    KNOWN_DISTANCE = 12.0
-    KNOWN_PIXELS = 135.5
-    KNOWN_HEIGHT = 424.0
+    KNOWN_WIDTH = 17.0
+    KNOWN_DISTANCE = 132.0
+    KNOWN_PIXELS = 26.0
+    KNOWN_ALTITUDE = 50.0
 
     focal_length = (KNOWN_PIXELS * KNOWN_DISTANCE)/KNOWN_WIDTH
-    #hypotenuse = (KNOWN_WIDTH * focal_length)/width
-    distance = (KNOWN_WIDTH * focal_length)/width
+    hypotenuse = (KNOWN_WIDTH * focal_length)/width
+#    distance = (KNOWN_WIDTH * focal_length)/width
 
     #0.2125 degrees per pixel vertical
-#    theta = (0.2125) * (240 - y)
+    theta = ((0.2125) * (240 - y)) - 25 #magic!
+
+    print("theta")
+    print(theta)
     
-#    distance = KNOWN_HEIGHT * (math.tan((math.pi / 2) - math.radians(theta)))
+    distance = KNOWN_ALTITUDE * (math.tan((math.pi / 2) - math.radians(theta)))
     
     return distance
 
+    
+
+    
 x = True
 while x:
     print("while loop entered")
     img = get_image()
+#    cv2.imwrite("img.jpg", img)
     print("image gotten")
     center_point = [160, 120]
-    file = open('output.txt', 'a')
     try:
+        file = open('/home/pi/python/output.txt', 'a')
         current_point, size, y = pipeline.process(img)
         #negative means turn left, positive means turn right
         pixel_difference = center_point[0] - current_point[0]
@@ -73,7 +87,7 @@ while x:
         n.angle_difference = angle_difference     
         target_width = size[0]
         target_height = size[1]
-        distance = find_distance(target_width, target_height, y)
+        distance = find_distance(target_width, y)
         n.distance_from_target = distance
         print("angle")
         file.write("angle: ")
@@ -84,6 +98,7 @@ while x:
         print(distance)
         file.write(str(distance))
         file.write("\n")
+        file.close()
     except UnboundLocalError:
         print(":(")
     except (TypeError, cv2.error) as e:
